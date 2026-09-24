fce4e618d6413e7957e277f78a2c36af0f7c613f
Yes-07 <nichola.rucita12@gmail.com>
2026-09-24 20:59:54 +0700

    feat(landing): Made the Theme section.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Chair from "@/src/modules/landing/chair"
+import Theme from "@/src/modules/landing/theme"
 
 export default function Home() {
   return (
     /*main tag is tempoary container*/
     <main className="*:px-7 *:py-5.5 md:*:px-30 md:*:py-8">
       <Chair />
+      <Theme />
     </main>
   )
 }
